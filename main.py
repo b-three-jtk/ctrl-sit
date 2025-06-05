82ca29df39f2a246d13a9f5504403b81809cd5e3
dirarmdn <dhiraramadini@gmail.com>
2025-06-05 20:30:02 +0700

    feat: test integration with sound alert

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 import mediapipe as mp
 import time
 import math as m
+import posture_alert as pa
 
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
@@ -10,7 +11,6 @@ mp_drawing = mp.solutions.drawing_utils
 
 good_frames = 0
 bad_frames = 0
-send_alert = False
 fps = 30  # asumsi sementara
 
 def detect_posture(image, pose, fps, good_frames, bad_frames):
@@ -25,21 +25,23 @@ def detect_posture(image, pose, fps, good_frames, bad_frames):
         l_shoulder = lm[mp_pose.PoseLandmark.LEFT_SHOULDER.value]
         r_shoulder = lm[mp_pose.PoseLandmark.RIGHT_SHOULDER.value]
         l_hip = lm[mp_pose.PoseLandmark.LEFT_HIP.value]
+        r_hip = lm[mp_pose.PoseLandmark.RIGHT_HIP.value]
         l_ear = lm[mp_pose.PoseLandmark.LEFT_EAR.value]
+        r_ear = lm[mp_pose.PoseLandmark.RIGHT_EAR.value]
 
-        shouder_x = int((l_shoulder.x + r_shoulder.x) / 2 * width)
-        shouder_y = int((l_shoulder.y + r_shoulder.y) / 2 * height)
-        ear_x = int(l_ear.x * width)
-        ear_y = int(l_ear.y * height)
-        hip_x = int(l_hip.x * width)
-        hip_y = int(l_hip.y * height)
+        shoulder_x = int((l_shoulder.x + r_shoulder.x) / 2 * width)
+        shoulder_y = int((l_shoulder.y + r_shoulder.y) / 2 * height)
+        ear_x = int((l_ear.x + r_ear.x) / 2 * width)
+        ear_y = int((l_ear.y + r_ear.y) / 2 * height)
+        hip_x = int((l_hip.x + r_hip.x) / 2 * width)
+        hip_y = int((l_hip.y + r_hip.y) / 2 * height)
 
-        cv2.circle(image_bgr, (shouder_x, shouder_y), 7, (0, 255, 0), -1)
+        cv2.circle(image_bgr, (shoulder_x, shoulder_y), 7, (0, 255, 0), -1)
         cv2.circle(image_bgr, (ear_x, ear_y), 7, (0, 255, 0), -1)
         cv2.circle(image_bgr, (hip_x, hip_y), 7, (0, 255, 0), -1)
 
-        neck_angle = m.degrees(m.atan2(ear_y - shouder_y, ear_x - shouder_x))
-        torso_angle = m.degrees(m.atan2(hip_y - shouder_y, hip_x - shouder_x))
+        neck_angle = abs(m.degrees(m.atan2(shoulder_y - ear_y, shoulder_x - ear_x)))
+        torso_angle = abs(m.degrees(m.atan2(hip_y - shoulder_y, hip_x - shoulder_x)))
 
         neck_angle = abs(neck_angle - 90)
         torso_angle = abs(torso_angle - 90)
@@ -67,7 +69,9 @@ def detect_posture(image, pose, fps, good_frames, bad_frames):
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         if bad_time > 20:
-            send_alert = True
+            if not st.session_state.get('alert_sent', False):
+                pa.handle_posture_audio(True)
+                st.session_state['alert_sent'] = True
 
     return image_bgr, good_frames, bad_frames
 
@@ -76,7 +80,7 @@ st.set_page_config(page_title="Ctrl+Sit - Posture Monitor", layout="centered")
 st.title("🪑 Ctrl+Sit - Real-time Posture Detection")
 st.markdown("Mulai kamera dan pastikan postur dudukmu tetap baik.")
 
-run = st.checkbox('Start Camera')
+run = st.button('Start Camera')
 frame_window = st.image([])
 
 cap = None
